0d875883007f266e87c2e652ec4eac3e1d2ab034
SebastianSJM <59491126+SebastianSJM@users.noreply.github.com>
2020-11-17 19:40:56 -0500

    Add files via upload

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-var slideIndex = 0;
+/*var slideIndex = 0;
 var bg = "bg_login";
 var slidesNumber = 3;
 showSlides();
@@ -9,4 +9,4 @@ function showSlides() {
 		document.getElementById('main').style.background = "url(public/img/bg_login" + slideIndex +".jpg) no-repeat 0px 0px"; 
 		document.getElementById('main').style.backgroundSize = "cover";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
+}*/
